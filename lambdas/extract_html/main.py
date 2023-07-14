f3f4c1c51a5c492dbddfab40067dcefcb0282d80
Diego Perez <pharesdiego@gmail.com>
2023-07-13 19:20:40 -0500

    feat: fetch html and save to raw data bucket
    HTML objects will be saved with a key based on the source and the
    current UTC datetime

--- lambdas/extract_html/main.py
+++ lambdas/extract_html/main.py
@@ -1,14 +1,49 @@
 import requests
 import os
+import boto3
+import io
+import logging
+import re
+from datetime import datetime
+from botocore.exceptions import ClientError
 
-def handler(event, context):
-    url_to_fetch = event['url']
-    response = requests.get(url_to_fetch)
+
+def get_host_from_url(url):
+    host = re.match(r'(\w+)\.com', url).group(1)
+
+    return host
+
+
+def fetch_url(url):
+    response = requests.get(url)
 
     if (response.status_code != 200):
         raise Exception('could not fetch url')
-    
+
     if (response.text == ''):
         raise Exception('response is empty')
 
     return response.text
+
+
+def handler(event, context):
+    url_to_fetch = event['url']
+    fetched_data = fetch_url(url_to_fetch)
+
+    try:
+        generated_key = f'{get_host_from_url(url_to_fetch)}-{datetime.utcnow().strftime("%d-%m-%Y %H-%M-%S")}.html'
+        temp_file = io.StringIO(fetched_data)
+
+        s3_client = boto3.client('s3')
+        s3_client.put_object(
+            Body=temp_file.getvalue(),
+            Bucket=os.environ['RAW_DATA_BUCKET'],
+            Key=generated_key
+        )
+
+        temp_file.close()
+    except ClientError as e:
+        logging.error(e)
+        return False
+
+    return generated_key
